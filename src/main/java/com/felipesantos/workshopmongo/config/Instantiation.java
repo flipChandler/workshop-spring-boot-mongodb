bbbfea88c97b5d6eed2a4c2125448e1b854fbd23
Felipe Santos <felipe.santos11@hotmail.com>
2025-07-01 19:58:28 -0300

    Database Instantiation

---
+++ src/main/java/com/felipesantos/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.felipesantos.workshopmongo.config;
+
+import com.felipesantos.workshopmongo.domain.User;
+import com.felipesantos.workshopmongo.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    public Instantiation(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
